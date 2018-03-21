463db35833c98b05ac8d0a6f3febeefc61ea35be
Conrad Weiser <vibrantclouds@protonmail.com>
2018-03-21 11:09:11 -0400

    Number counter now uses correct datatype
    Previously was unsigned, although that didn't account for people who managed to get negative numbers. (1d20-99999)
    Came to the conclusion that it's safe to sign the number, as it has an upper bound of (999d999+99999) --> ((999*999)+99999) which is still below the integer maximum.

--- Plugins/Public/playercntl_plugin/MiscCmds.cpp
+++ Plugins/Public/playercntl_plugin/MiscCmds.cpp
@@ -230,7 +230,8 @@ namespace MiscCmds
 	Roll dice for everyone within 6km of a vessel. Supports 1d20 formatting.
 	*/
 	bool MiscCmds::UserCmd_Dice(uint iFromClientID, const wstring &wscCmd, const wstring &wscParam, const wchar_t *usage)
-	{
+	{
+
 		boost::wregex expr(L"(\\d{1,2})[Dd](\\d{1,3})(([+\\-*])?(\\d{1,5}))?");
 		boost::wsmatch sm;
 
@@ -264,7 +265,7 @@ namespace MiscCmds
 			}
 
 			string diceResultSteps = "";
-			uint number = 0;
+			int number = 0;
 
 			for (int i = 0; i < rollCount; i++)
 			{
@@ -316,8 +317,10 @@ namespace MiscCmds
 			PrintUserCmdText(iFromClientID, L"Usage: /roll 1d20");
 			PrintUserCmdText(iFromClientID, L"       /roll 1d8+4");
 			PrintUserCmdText(iFromClientID, L"       /roll 4d20+2");
-		}
-		return true;
+		}
+
+		return true;
+
 	}
 
 	/** Throw the dice and tell all players within 6 km */

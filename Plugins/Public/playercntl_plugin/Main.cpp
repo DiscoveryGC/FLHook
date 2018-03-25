a40923bedbedef01c67de7a8ad9af50f0ab00282
Conrad Weiser <bluecrecent0@gmail.com>
2018-03-25 17:30:49 -0400

    Old /dice command aliased to the new command.

--- Plugins/Public/playercntl_plugin/Main.cpp
+++ Plugins/Public/playercntl_plugin/Main.cpp
@@ -903,6 +903,7 @@ USERCMD UserCmds[] =
 	{ L"/pos",			MiscCmds::UserCmd_Pos,			L"Usage: /pos" },
 	{ L"/stuck",		MiscCmds::UserCmd_Stuck,		L"Usage: /stuck" },
 	{ L"/droprep",		MiscCmds::UserCmd_DropRep,		L"Usage: /droprep" },
+	{ L"/dice",			MiscCmds::UserCmd_Dice,			L"Usage: /dice 1d20 | 1d20+3 | etc."},
 	{ L"/roll",			MiscCmds::UserCmd_Dice,			L"Usage: /roll 1d20 | 1d20+3 | etc." },
 	{ L"/coin",			MiscCmds::UserCmd_Coin,			L"Usage: /coin" },
 	{ L"/pimpship",		PimpShip::UserCmd_PimpShip,		L"Usage: /pimpship" },

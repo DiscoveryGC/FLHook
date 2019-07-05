337ad2864f6f727de38c7968b5a2cabbcd7c3189
lnvoker <51702338+lnvoker@users.noreply.github.com>
2019-07-05 21:06:49 +0300

    HookExt exports fix
    Now the plugin exports two more functions correctly.

--- Plugins/Public/hookext_plugin/hookext_exports.h
+++ Plugins/Public/hookext_plugin/hookext_exports.h
@@ -1,13 +1,13 @@
 namespace HookExt
 {
 	IMPORT wstring IniGetWS(uint client, const string &name);
-	IMPORT wstring IniGetW(uint client, const string &name);
+	IMPORT string IniGetS(uint client, const string &name);
 	IMPORT uint IniGetI(uint client, const string &name);
 	IMPORT bool IniGetB(uint client, const string &name);
 	IMPORT float IniGetF(uint client, const string &name);
 
 	IMPORT void IniSetWS(uint client, const string &name, const wstring &value);
-	IMPORT void IniSetS(uint client, const string &name, const string *value);
+	IMPORT void IniSetS(uint client, const string &name, const string &value);
 	IMPORT void IniSetI(uint client, const string &name, uint value);
 	IMPORT void IniSetB(uint client, const string &name, bool value);
 	IMPORT void IniSetF(uint client, const string &name, float value);

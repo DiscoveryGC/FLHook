e569edb0e3554b990a2a609667f03be88b6859b3
lnvoker <51702338+lnvoker@users.noreply.github.com>
2019-07-13 02:15:41 +0300

    Update Plugins/Public/playercntl_plugin/Main.cpp
    Co-Authored-By: Alex <18709025+dsyalex@users.noreply.github.com>

--- Plugins/Public/playercntl_plugin/Main.cpp
+++ Plugins/Public/playercntl_plugin/Main.cpp
@@ -918,7 +918,7 @@ USERCMD UserCmds[] =
 	{ L"/showrestarts",	Restart::UserCmd_ShowRestarts,	L"Usage: /showrestarts" },
 	{ L"/givecash",		GiveCash::UserCmd_GiveCash,		L"Usage: /givecash <charname> <cash> [anon] [commentary] or /gc ..." },
 	{ L"/gc",			GiveCash::UserCmd_GiveCash,		L"Usage: /givecash <charname> <cash> [anon] [commentary] or /gc ..." },
-	{ L"/givecasht",	GiveCash::UserCmd_GiveCashTarget,		L"Usage: /givecasht <cash> [anon] [commentary] or /gct ..." },
+	{ L"/givecasht",	GiveCash::UserCmd_GiveCashTarget,		L"Usage: /givecasht <cash> [anon] [comment] or /gct ..." },
 	{ L"/gct",			GiveCash::UserCmd_GiveCashTarget,		L"Usage: /givecasht <cash> [anon] [comment] or /gct ..." },
 	{ L"/sendcash",		GiveCash::UserCmd_GiveCash,		L"Usage: /givecash <charname> <cash> [anon] [comment]"},
 	{ L"/set cashcode",	GiveCash::UserCmd_SetCashCode,	L"Usage: /set cashcode <code>"},

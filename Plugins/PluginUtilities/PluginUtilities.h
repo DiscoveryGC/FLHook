d769c4f79932463eee7aba3eacc9a4bbd83f153e
lnvoker <51702338+lnvoker@users.noreply.github.com>
2019-07-21 00:12:47 +0300

    pluginutilities: PluginUtilities.h

--- Plugins/PluginUtilities/PluginUtilities.h
+++ Plugins/PluginUtilities/PluginUtilities.h
@@ -9,6 +9,8 @@
 
 #define HAS_FLAG(a, b) ((a).wscFlags.find(b) != -1)
 
+#include "DynPacket.h"
+
 size_t WstrInsensitiveFind(wstring haystack, wstring needle);
 bool IsInRange(uint iClientID, float fDistance);
 

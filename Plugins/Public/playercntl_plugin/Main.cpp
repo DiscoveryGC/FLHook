c6158766e2408babfa37bb47325b9e8e666ab39c
lnvoker <51702338+lnvoker@users.noreply.github.com>
2019-09-21 11:52:31 +0300

    Main.cpp

--- Plugins/Public/playercntl_plugin/Main.cpp
+++ Plugins/Public/playercntl_plugin/Main.cpp
@@ -1407,14 +1407,6 @@ bool ExecuteCommandString_Callback(CCmds* cmds, const wstring &wscCmd)
 		HyperJump::AdminCmd_Chase(cmds, cmds->ArgCharname(1));
 		return true;
 	}
-	/*
-	else if (IS_CMD("testbot"))
-	{
-		returncode = SKIPPLUGINS_NOFUNCTIONCALL;
-		HyperJump::AdminCmd_TestBot(cmds, cmds->ArgStr(1), cmds->ArgInt(2));
-		return true;
-	}
-	*/
 	else if (IS_CMD("lrs"))
 	{
 		returncode = SKIPPLUGINS_NOFUNCTIONCALL;
@@ -1427,13 +1419,6 @@ bool ExecuteCommandString_Callback(CCmds* cmds, const wstring &wscCmd)
 		HyperJump::AdminCmd_MakeCoord(cmds);
 		return true;
 	}
-	else if (IS_CMD("jumptest"))
-	{
-		returncode = SKIPPLUGINS_NOFUNCTIONCALL;
-		HyperJump::AdminCmd_JumpTest(cmds, cmds->ArgStr(1));
-		return true;
-	}
-	
 	else if (IS_CMD("authchar"))
 	{
 		returncode = SKIPPLUGINS_NOFUNCTIONCALL;

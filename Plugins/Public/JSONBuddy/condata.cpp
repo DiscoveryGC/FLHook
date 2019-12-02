c6566dff93ca464cf6c92f31b23d1c741e6a7232
Alex <dimension-@live.co.uk>
2019-12-02 00:08:05 +0000

    pingtarget: Remove target ping milliseconds due to potential for getting player location

--- Plugins/Public/JSONBuddy/condata.cpp
+++ Plugins/Public/JSONBuddy/condata.cpp
@@ -493,8 +493,7 @@ bool Condata::UserCmd_PingTarget(uint iClientID, const wstring &wscCmd, const ws
 	if (ConData[iClientIDTarget].lstPing.size() < set_iPingKickFrame)
 		Response += L"n/a Fluct: n/a ";
 	else {
-		Response += stows(itos(ConData[iClientIDTarget].iAveragePing)).c_str();
-		Response += L"ms ";
+		Response += L"[target ping data redacted due to privacy concerns] ";
 		if (set_iPingKick > 0) {
 			Response += L"(Max: ";
 			Response += stows(itos(set_iPingKick)).c_str();

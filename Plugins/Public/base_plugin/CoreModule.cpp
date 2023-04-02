7bcd3f3ed24795bf1308ad7990e7bc3fd80e8a11
Aingar <47479741+Aingar@users.noreply.github.com>
2023-04-02 21:14:25 +0200

    changes lost in merging

--- Plugins/Public/base_plugin/CoreModule.cpp
+++ Plugins/Public/base_plugin/CoreModule.cpp
@@ -359,7 +359,7 @@ void CoreModule::Spawn()
 		if (shield_reinforcement_threshold_map.count(base->base_level))
 			base->base_shield_reinforcement_threshold = shield_reinforcement_threshold_map[base->base_level];
 		else
-			base_shield_reinforcement_threshold = FLT_MAX;
+			base->base_shield_reinforcement_threshold = FLT_MAX;
 
 		base->SyncReputationForBaseObject(space_obj);
 		if (set_plugin_debug > 1)
@@ -582,7 +582,7 @@ float CoreModule::SpaceObjDamaged(uint space_obj, uint attacking_space_obj, floa
 		return curr_hitpoints;
 	}
 
-	if (shield_strength_multiplier >= 1.0f || isGlobalBaseInvulnerabilityActive || base->invulnerable == 1)
+	if (base->shield_strength_multiplier >= 1.0f || isGlobalBaseInvulnerabilityActive || base->invulnerable == 1)
 	{
         // base invulnerable, keep current health value
         return curr_hitpoints;
@@ -593,12 +593,12 @@ float CoreModule::SpaceObjDamaged(uint space_obj, uint attacking_space_obj, floa
         return new_hitpoints;
     }
     
-    float damageTaken = ((curr_hitpoints - new_hitpoints) * (1 - shield_strength_multiplier));
+    float damageTaken = ((curr_hitpoints - new_hitpoints) * (1 - base->shield_strength_multiplier));
 
-    damage_taken_since_last_threshold += damageTaken;
-    if (damage_taken_since_last_threshold >= base_shield_reinforcement_threshold) {
-        damage_taken_since_last_threshold -= base_shield_reinforcement_threshold;
-        shield_strength_multiplier += shield_reinforcement_increment;
+    base->damage_taken_since_last_threshold += damageTaken;
+    if (base->damage_taken_since_last_threshold >= base->base_shield_reinforcement_threshold) {
+        base->damage_taken_since_last_threshold -= base->base_shield_reinforcement_threshold;
+        base->shield_strength_multiplier += shield_reinforcement_increment;
     }
     
     return curr_hitpoints - damageTaken;

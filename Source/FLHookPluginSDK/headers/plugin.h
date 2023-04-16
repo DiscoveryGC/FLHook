0f100b035643959168c4e7b3090e1ce7fe003649
Aingar <47479741+Aingar@users.noreply.github.com>
2023-04-16 13:23:15 +0200

    mergefix: removal of duplicate struct

--- Source/FLHookPluginSDK/headers/plugin.h
+++ Source/FLHookPluginSDK/headers/plugin.h
@@ -374,14 +374,6 @@ struct CUSTOM_JUMP_CALLOUT_STRUCT
 	uint jumpType = 0;
 };
 
-struct CUSTOM_JUMP_CALLOUT_STRUCT
-{
-	uint iClientID;
-	uint iSystemID;
-	Vector pos;
-	Matrix ori;
-	uint jumpType = 0;
-};
 struct CUSTOM_REVERSE_TRANSACTION_STRUCT
 {
 	uint iClientID;

19e7bd09c16cea1ac2edd91e42576d7db939e94e
CzaroMain <mierzei@gmail.com>
2023-05-08 19:14:06 +0200

    Siege Hotfix
    - Fixed a case where base takes no damage from player fire if shield generator is not present or without fuel
    - rewritten Main.cpp::HkCb_AddDmgEntry and CoreModule::SpaceObjDamaged methods for better readability.
    - micro optimizations

--- Plugins/Public/base_plugin/CoreModule.cpp
+++ Plugins/Public/base_plugin/CoreModule.cpp
@@ -359,7 +359,7 @@ void CoreModule::Spawn()
 		if (shield_reinforcement_threshold_map.count(base->base_level))
 			base->base_shield_reinforcement_threshold = shield_reinforcement_threshold_map[base->base_level];
 		else
-			base->base_shield_reinforcement_threshold = 0.0f;
+			base_shield_reinforcement_threshold = FLT_MAX;
 
 		base->SyncReputationForBaseObject(space_obj);
 		if (set_plugin_debug > 1)
@@ -578,22 +578,30 @@ float CoreModule::SpaceObjDamaged(uint space_obj, uint attacking_space_obj, floa
 
 	if (base->shield_state != PlayerBase::SHIELD_STATE_OFFLINE && base->shield_strength_multiplier < 1.0 && !isGlobalBaseInvulnerabilityActive && base->invulnerable == 0)
 	{
-		float damageTaken = ((curr_hitpoints - new_hitpoints) * (1 - base->shield_strength_multiplier));
-
-		base->damage_taken_since_last_threshold += damageTaken;
-		if (base->damage_taken_since_last_threshold >= base->base_shield_reinforcement_threshold) {
-			base->damage_taken_since_last_threshold -= base->base_shield_reinforcement_threshold;
-			base->shield_strength_multiplier += shield_reinforcement_increment;
-		}
-
-		return curr_hitpoints - damageTaken;
-	}
-	else
-	{
+		//force the base to keep current health
 		return curr_hitpoints;
 	}
 
-	return 0.0f;
+	if (shield_strength_multiplier >= 1.0f || isGlobalBaseInvulnerabilityActive || base->invulnerable == 1)
+	{
+        // base invulnerable, keep current health value
+        return curr_hitpoints;
+	}
+    
+    if(base->shield_state == PlayerBase::SHIELD_STATE_OFFLINE){
+        // shield offline, return expected damage without modifications
+        return new_hitpoints;
+    }
+    
+    float damageTaken = ((curr_hitpoints - new_hitpoints) * (1 - shield_strength_multiplier));
+
+    damage_taken_since_last_threshold += damageTaken;
+    if (damage_taken_since_last_threshold >= base_shield_reinforcement_threshold) {
+        damage_taken_since_last_threshold -= base_shield_reinforcement_threshold;
+        shield_strength_multiplier += shield_reinforcement_increment;
+    }
+    
+    return curr_hitpoints - damageTaken;
 }
 
 bool CoreModule::SpaceObjDestroyed(uint space_obj)

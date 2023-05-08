afc5806b88ea21482b7b681c48b74b879e67dd0c
Aingar <47479741+Aingar@users.noreply.github.com>
2023-05-08 19:37:31 +0200

    Siege hotfix: fixing tab vs space gore

--- Plugins/Public/base_plugin/CoreModule.cpp
+++ Plugins/Public/base_plugin/CoreModule.cpp
@@ -359,7 +359,7 @@ void CoreModule::Spawn()
 		if (shield_reinforcement_threshold_map.count(base->base_level))
 			base->base_shield_reinforcement_threshold = shield_reinforcement_threshold_map[base->base_level];
 		else
-			base_shield_reinforcement_threshold = FLT_MAX;
+			base->base_shield_reinforcement_threshold = FLT_MAX;
 
 		base->SyncReputationForBaseObject(space_obj);
 		if (set_plugin_debug > 1)
@@ -584,24 +584,24 @@ float CoreModule::SpaceObjDamaged(uint space_obj, uint attacking_space_obj, floa
 
 	if (shield_strength_multiplier >= 1.0f || isGlobalBaseInvulnerabilityActive || base->invulnerable == 1)
 	{
-        // base invulnerable, keep current health value
-        return curr_hitpoints;
+		// base invulnerable, keep current health value
+		return curr_hitpoints;
 	}
-    
-    if(base->shield_state == PlayerBase::SHIELD_STATE_OFFLINE){
-        // shield offline, return expected damage without modifications
-        return new_hitpoints;
-    }
-    
-    float damageTaken = ((curr_hitpoints - new_hitpoints) * (1 - shield_strength_multiplier));
+	
+	if(base->shield_state == PlayerBase::SHIELD_STATE_OFFLINE){
+		// shield offline, return expected damage without modifications
+		return new_hitpoints;
+	}
+	
+	float damageTaken = ((curr_hitpoints - new_hitpoints) * (1 - shield_strength_multiplier));
 
-    damage_taken_since_last_threshold += damageTaken;
-    if (damage_taken_since_last_threshold >= base_shield_reinforcement_threshold) {
-        damage_taken_since_last_threshold -= base_shield_reinforcement_threshold;
-        shield_strength_multiplier += shield_reinforcement_increment;
-    }
-    
-    return curr_hitpoints - damageTaken;
+	damage_taken_since_last_threshold += damageTaken;
+	if (damage_taken_since_last_threshold >= base_shield_reinforcement_threshold) {
+		damage_taken_since_last_threshold -= base_shield_reinforcement_threshold;
+		shield_strength_multiplier += shield_reinforcement_increment;
+	}
+	
+	return curr_hitpoints - damageTaken;
 }
 
 bool CoreModule::SpaceObjDestroyed(uint space_obj)

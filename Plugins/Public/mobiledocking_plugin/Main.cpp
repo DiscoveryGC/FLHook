7fdf40447dd8b2b14dcecd8aa89271e8eb002d5e
CzaroMain <mierzei@gmail.com>
2023-06-07 15:10:06 +0200

    Merge fix

--- Plugins/Public/mobiledocking_plugin/Main.cpp
+++ Plugins/Public/mobiledocking_plugin/Main.cpp
@@ -586,7 +586,7 @@ int __cdecl Dock_Call(unsigned int const &iShip, unsigned int const &iBaseID, in
 	{
 		// If target not a player in FREIGHTER class ship, ignore request
 		uint iType;
-		pub::SpaceObj::GetType(iTargetShip, iType);
+		pub::SpaceObj::GetType(iBaseID, iType);
 		if (!(iType & (OBJ_FREIGHTER | OBJ_TRANSPORT | OBJ_GUNBOAT | OBJ_CRUISER | OBJ_CAPITAL)))
 			return 0;
 

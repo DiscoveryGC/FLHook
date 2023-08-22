e63ccb260a62a405607fac65cfc8a5de56e235c6
CzaroMain <mierzei@gmail.com>
2023-08-22 19:59:21 +0200

    CLootRoadkill: Made speed configurable, changed default to 25

--- Plugins/Public/playercntl_plugin/Main.cpp
+++ Plugins/Public/playercntl_plugin/Main.cpp
@@ -52,6 +52,8 @@ float set_iDockBroadcastRange = 9999;
 float set_fSpinProtectMass;
 float set_fSpinImpulseMultiplier;
 
+float set_fMinCLootRoadkillSpeed = 25.0f;
+
 // set of ships which cannot use TradeLane, and are blocked
 // from forming on other ships to bypass the block
 unordered_set<uint> setLaneAndFormationBannedShips;
@@ -124,6 +126,8 @@ void LoadSettings()
 
 	set_iLocalChatRange = IniGetF(scPluginCfgFile, "General", "LocalChatRange", 0);
 	set_iDockBroadcastRange = IniGetF(scPluginCfgFile, "General", "DockBroadcastRange", 0);
+	
+	set_fMinCLootRoadkillSpeed = IniGetF(scPluginCfgFile, "General", "MinCLootRoadkillSpeed", 25.0f);
 
 	set_bLocalTime = IniGetB(scPluginCfgFile, "General", "LocalTime", false);
 
@@ -602,7 +606,7 @@ namespace HkIServerImpl
 			Vector V1, V2;
 			pub::SpaceObj::GetMotion(client_ship, V1, V2);
 			float playerSpeed = sqrtf(V1.x * V1.x + V1.y * V1.y + V1.z * V1.z);
-			if (playerSpeed > 35)
+			if (playerSpeed > set_fMinCLootRoadkillSpeed)
 			{
 				pub::SpaceObj::Destroy(ci.dwTargetShip, DestroyType::FUSE);
 				returncode = SKIPPLUGINS_NOFUNCTIONCALL;

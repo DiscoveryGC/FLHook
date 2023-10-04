f489aee246ab6fca9d96fe224b27ee57c443071d
CzaroMain <mierzei@gmail.com>
2023-10-04 14:17:51 +0200

    PVEController: NPCdrops can now randomize amount.

--- Plugins/Public/pvecontroller/Main.cpp
+++ Plugins/Public/pvecontroller/Main.cpp
@@ -43,7 +43,8 @@ struct stBountyBasePayout {
 struct stDropInfo {
 	uint uGoodID;
 	float fChance;
-	uint uAmountDropped;
+	uint uAmountDroppedMin;
+	uint uAmountDroppedMax;
 };
 
 struct stWarzone {
@@ -287,11 +288,12 @@ void LoadSettingsNPCDrops()
 						string szGood = ini.get_value_string(1);
 						drop.uGoodID = CreateID(szGood.c_str());
 						drop.fChance = ini.get_value_float(2);
-						drop.uAmountDropped = ini.get_value_int(3);
-						if (drop.uAmountDropped == 0)
+						drop.uAmountDroppedMin = ini.get_value_int(3);
+						if (drop.uAmountDroppedMin == 0)
 						{
-							drop.uAmountDropped = 1;
+							drop.uAmountDroppedMin = 1;
 						}
+						drop.uAmountDroppedMax = ini.get_value_int(4);
 						mmapDropInfo.insert(make_pair(iClass, drop));
 						++iLoadedNPCDropClasses;
 						if (set_iPluginDebug)
@@ -729,14 +731,24 @@ void __stdcall HkCb_ShipDestroyed(DamageList* dmg, DWORD* ecx, uint iKill)
 	const auto& iterEnd = mmapDropInfo.upper_bound(victimShiparch->iShipClass);
 	while (iter != iterEnd)
 	{
+		const auto& dropData = iter->second;
 		float roll = static_cast<float>(rand()) / RAND_MAX;
-		if (roll < iter->second.fChance)
+		if (roll < dropData.fChance)
 		{
 			Vector vLoc;
 			Matrix mRot;
 			pub::SpaceObj::GetLocation(iVictimShipId, vLoc, mRot);
 			vLoc.x += 30.0;
-			Server.MineAsteroid(uKillerSystem, vLoc, set_uLootCrateID, iter->second.uGoodID, iter->second.uAmountDropped, iKillerClientId);
+			uint finalAmount;
+			if (dropData.uAmountDroppedMax)
+			{
+				finalAmount = dropData.uAmountDroppedMin + (rand() % (dropData.uAmountDroppedMax - dropData.uAmountDroppedMin + 1));
+			}
+			else
+			{
+				finalAmount = dropData.uAmountDroppedMin;
+			}
+			Server.MineAsteroid(uKillerSystem, vLoc, set_uLootCrateID, dropData.uGoodID, finalAmount, iKillerClientId);
 		}
 		iter++;
 	}

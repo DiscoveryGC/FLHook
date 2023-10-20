b2f17930223da584802d29a8144dd2bdbbc85015
CzaroMain <mierzei@gmail.com>
2023-10-21 00:35:36 +0200

    micro optimization

--- Plugins/Public/base_plugin/CoreModule.cpp
+++ Plugins/Public/base_plugin/CoreModule.cpp
@@ -220,7 +220,20 @@ bool CoreModule::Timer(uint time)
 		SetShieldState(base->shield_state);
 	}
 
-	if ((time % set_tick_time) != 0 || set_holiday_mode)
+	// we need to periodically set the health of all POBs to trigger a clientside 'refresh'
+	// this allows clients to perceive those objects as dockable
+	if ((time % 5) == 0)
+	{
+		float rhealth = base->base_health / base->max_base_health;
+		pub::SpaceObj::SetRelativeHealth(space_obj, rhealth);
+	}
+
+	if (set_holiday_mode)
+	{
+		return false;
+	}
+
+	if ((time % set_tick_time) != 0)
 	{
 		return false;
 	}

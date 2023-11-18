2b092619f4678a74550727f8311d69fcf02dfbda
Alex <18709025+dsyalex@users.noreply.github.com>
2023-11-18 16:59:32 +0000

    Add BountyScan plugin (#350)
    * Add BountyScan plugin
    
    * Requested changes
    
    ---------
    
    Co-authored-by: Alex <alex@discoverygc.com>

---
+++ Plugins/Public/bountyscan/Main.h
@@ -0,0 +1,13 @@
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <math.h>
+#include <list>
+#include <map>
+#include <algorithm>
+#include <FLHook.h>
+#include <plugin.h>
+#include <PluginUtilities.h>
+
+using namespace std;
